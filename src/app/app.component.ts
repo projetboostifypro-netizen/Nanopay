b15aef32d7123adda5a386ebfa93d0f8820cf34f
projetboostifypro-netizen <projetboostifypro@gmail.com>
2026-01-31 14:06:22 +0100

    Supprimer tous les anciens fichiers

--- src/app/app.component.ts
+++
@@ -1,7 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-root',
-  template: `<iframe src="https://https://west-african-card.vercel.app/" style="width:100%;height:100%;border:none;"></iframe>`
-})
-export class AppComponent {}
